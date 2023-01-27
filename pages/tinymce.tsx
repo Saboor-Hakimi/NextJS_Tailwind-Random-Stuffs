e0d30438c102e156b14b902685c2373f8dcfcc3a
Saboor Hakimi <44523544+Saboor-Hakimi@users.noreply.github.com>
2023-01-28 02:05:14 +0430

    tinymce

---
+++ pages/tinymce.tsx
@@ -0,0 +1,59 @@
+import { NextPage } from "next";
+
+import { Editor } from "@tinymce/tinymce-react";
+import { useRef } from "react";
+
+const TinyMCEPage: NextPage = () => {
+  const editorRef = useRef(null);
+  const log = () => {
+    if (editorRef.current) {
+      console.log(editorRef.current.getContent());
+    }
+  };
+
+  return (
+    <div>
+      <Editor
+        apiKey={`${process.env.NEXT_PUBLIC_TINY_API_KEY}`}
+        onInit={(evt, editor) => {
+          return (editorRef.current = editor);
+        }}
+        initialValue="<p>This is the initial content of the editor.</p>"
+        init={{
+          height: 500,
+          menubar: false,
+          plugins: [
+            "advlist",
+            "autolink",
+            "lists",
+            "link",
+            "image",
+            "charmap",
+            "preview",
+            "anchor",
+            "searchreplace",
+            "visualblocks",
+            "code",
+            "fullscreen",
+            "insertdatetime",
+            "media",
+            "table",
+            "code",
+            "help",
+            "wordcount",
+          ],
+          toolbar:
+            "undo redo | blocks | " +
+            "bold italic forecolor | alignleft aligncenter " +
+            "alignright alignjustify | bullist numlist outdent indent | " +
+            "removeformat | help",
+          content_style:
+            "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
+        }}
+      />
+      <button onClick={log}>Log editor content</button>
+    </div>
+  );
+};
+
+export default TinyMCEPage;
