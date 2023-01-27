a60e2ab3bd6c5257dfc79efd8c5fbc5e3f465f29
Saboor Hakimi <44523544+Saboor-Hakimi@users.noreply.github.com>
2023-01-27 10:26:02 +0430

    random emoji api endpoint

---
+++ pages/api/index.tsx
@@ -0,0 +1,14 @@
+export default async (req, res) => {
+  const apiKey = req.query.apiKey;
+
+  //   validate the api key
+
+  const data = [getRandomEmoji(), getRandomEmoji(), getRandomEmoji()];
+  res.status(200).json({ data });
+};
+
+function getRandomEmoji() {
+  return String.fromCodePoint(
+    Math.floor(Math.random() * (0x1f600 - 0x1f300) + 0x1f300)
+  );
+}
---
+++ pages/stripe.tsx
@@ -0,0 +1,8 @@
+import { NextPage } from "next";
+import { useState, useId } from "react";
+
+const Home: NextPage = () => {
+  return <></>;
+};
+
+export default Home;
