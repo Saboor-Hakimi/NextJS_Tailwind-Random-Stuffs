2f19c85380e2d0f92a7237c9053292ab436217ca
Saboor Hakimi <44523544+Saboor-Hakimi@users.noreply.github.com>
2023-01-28 02:21:32 +0430

    quill

---
+++ pages/quill.tsx
@@ -0,0 +1,40 @@
+import { NextPage } from "next";
+import dynamic from "next/dynamic";
+
+const ReactQuill = dynamic(import("react-quill"), { ssr: false });
+import "react-quill/dist/quill.snow.css";
+import "react-quill/dist/quill.bubble.css";
+import { useState } from "react";
+
+const Home: NextPage = () => {
+  const [value, setValue] = useState("");
+
+  console.log(value);
+
+  const modules = {
+    toolbar: [
+      [{ font: [] }],
+      [{ header: [1, 2, 3, 4, 5, 6, false] }],
+      ["bold", "italic", "underline", "strike"],
+      [{ color: [] }, { background: [] }],
+      [{ script: "sub" }, { script: "super" }],
+      ["blockquote", "code-block"],
+      [{ list: "ordered" }, { list: "bullet" }],
+      [{ indent: "-1" }, { indent: "+1" }, { align: [] }],
+      ["link", "image", "video"],
+      ["clean"],
+    ],
+  };
+  return (
+    <>
+      <ReactQuill
+        theme="snow"
+        modules={modules}
+        onChange={setValue}
+        placeholder="Content goes here..."
+      />
+    </>
+  );
+};
+
+export default Home;
